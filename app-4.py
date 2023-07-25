9820bbb164edd960711a514e51d49e92d96850ee
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-07-25 19:14:21 +0700

    add memilih makan malam randomizer

--- app-4.py
+++ app-4.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.header('st.multiselect & st.checkbox')
 
@@ -16,17 +17,28 @@ st.subheader('st.checkbox')
 
 st.write('Pilih makan untuk makan malam !')
 
+# Define the checkboxes
 nasi_rendang = st.checkbox('Rendang')
 nasi_gule = st.checkbox('Gule')
 soto_ayam = st.checkbox('Soto Ayam')
 ayam_goreng_nashville = st.checkbox('Ayam Goreng Nashville')
 
+# Create a list of selected options
+selected_options = []
 if nasi_rendang:
-    st.write('You choose Rendang')
+    selected_options.append('Rendang')
 if nasi_gule:    
-    st.write('You choose Gule')
+    selected_options.append('Gule')
 if soto_ayam:    
-    st.write('You choose Soto Ayam')
+    selected_options.append('Soto Ayam')
 if ayam_goreng_nashville:
-    st.write('You choose Ayam Goreng Nashville')
-    
+    selected_options.append('Ayam Goreng Nashville')
+
+# Display the "Randomize" button
+if st.button('Randomize'):
+    # Randomly choose one option from the selected ones
+    if selected_options:
+        random_choice = random.choice(selected_options)
+        st.write(f'Anda memilih makan malam: {random_choice}')
+    else:
+        st.write('Silakan pilih makanan untuk makan malam!')
