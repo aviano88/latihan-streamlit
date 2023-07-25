53387779c786fc0ee203fdcf712f3c9a78b45ee3
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-07-25 19:03:02 +0700

    latihan checkbox dan multiselect

--- app-4.py
+++ app-4.py
@@ -1,10 +1,32 @@
 import streamlit as st
 
-st.header('st.multiselect')
+st.header('st.multiselect & st.checkbox')
 
+#example 1
+st.subheader('st.multiselect')
 options =  st.multiselect(
     'what are your favorite colors?',
     ['red', 'yellow', 'green', 'blue'], 
     ['red', 'yellow'])
 
 st.write('You favorite colors are', options)    
+
+#example 2
+st.subheader('st.checkbox')
+
+st.write('Pilih makan untuk makan malam !')
+
+nasi_rendang = st.checkbox('Rendang')
+nasi_gule = st.checkbox('Gule')
+soto_ayam = st.checkbox('Soto Ayam')
+ayam_goreng_nashville = st.checkbox('Ayam Goreng Nashville')
+
+if nasi_rendang:
+    st.write('You choose Rendang')
+if nasi_gule:    
+    st.write('You choose Gule')
+if soto_ayam:    
+    st.write('You choose Soto Ayam')
+if ayam_goreng_nashville:
+    st.write('You choose Ayam Goreng Nashville')
+    
